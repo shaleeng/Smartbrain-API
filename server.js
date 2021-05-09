28d955838b0ae66a47ff4783434a9337d0754df2
shaleeng <shaleengoel17@gmail.com>
2021-05-08 21:03:25 -0400

    set up

---
+++ server.js
@@ -0,0 +1,83 @@
+const e = require('express');
+const express = require('express')
+const bcrypt = require('bcrypt-nodejs')
+const cors = require('cors')
+const knex = require('knex')
+const register = require('./controllers/register')
+
+const db = knex({
+    client: 'pg',
+    connection: {
+        host : '127.0.0.1',
+        user : 'shaleengoel',
+        password : '',
+        database : 'smartbrain'
+    }
+});
+
+const app = express();
+app.use(express.json());
+app.use(cors())
+
+app.get('/', (req,res) => {
+    db.select('*').from('users')
+    .then(users => {
+        res.send(users)
+    })
+})
+
+app.post('/signin', (req, res) => {
+    db.select('email', 'hash').from('login').where({
+        email: req.body.email,
+    })
+    .then(data => {
+        const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+        if(isValid){
+            return db.select('*').from('users').where({
+                email: req.body.email,
+            })
+            .then(user =>{
+                res.json(user[0])
+            })
+            .catch(err => res.status(400).json('unable to get user'))
+        } else {
+            res.status(400).json('wrong credentials')
+        }
+    })
+    .catch(err => res.status(400).json('wrong credentials'))
+})
+
+app.post('/register', (req,res) => {register.handleRegister(req,res,db,bcrypt)})
+
+
+app.get('/profile/:id', (req,res) => {
+    const {id} = req.params;
+    db.select('*').from('users').where({id: id})
+    .then(user => {
+        if (user.length){
+            res.json(user);
+        } else {
+            res.status(400).json('not found');
+        }
+    })
+    .catch(err => {
+        res.status(400).json('Error getting user');
+    })
+})
+
+app.put('/image', (req,res) => {
+    const {id} = req.body;
+    db('users')
+    .where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+        res.json(entries[0]);
+    })
+    .catch(err => res.status(400).json('unable to get entries'))
+})
+
+
+app.listen(3000, () => {
+    console.log('app is running on port 3000')
+})
